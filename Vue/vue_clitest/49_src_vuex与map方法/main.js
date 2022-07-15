108bb720ff8733e9f52b565d514f05f7625a5b0b
Jove <2070721350@qq.com>
2022-07-15 21:30:50 +0800

    2022/7/15

---
+++ Vue/vue_clitest/49_src_vuex与map方法/main.js
@@ -0,0 +1,14 @@
+
+import Vue from 'vue'
+import App from './App.vue'
+
+// 引入 store
+import store from './vuex/store'
+
+Vue.config.productionTip = false
+
+new Vue({
+    el: '#app',
+    store,
+    render: h => h(App)
+})
